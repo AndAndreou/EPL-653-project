74905b8410aba3c2c5bc500f4a5fb3470d6e1888
mariokom <mkomod05@cs.ucy.ac.cy>
2015-04-29 19:40:06 +0300

    Added sound for bullet

--- 2dProjGroup/Assets/Scripts/EnergyBullet.cs
+++ 2dProjGroup/Assets/Scripts/EnergyBullet.cs
@@ -7,7 +7,8 @@ public class EnergyBullet : MonoBehaviour {
 	private Rigidbody bulletRigidBody;
 	private Dimension bulletDimension;
 	private GameRepository repository;
-	private float damage = 50.0f; //damage sferas 
+	private float damage = 50.0f; //damage sferas
+	private AudioSource sound;
 	
 	float xDif, zDif, yDif;
 	
@@ -34,6 +35,10 @@ public class EnergyBullet : MonoBehaviour {
 		
 		float yRotate = (90 * (int)bulletDimension) % 180;
 		transform.Rotate(new Vector3(0.0f,yRotate,0.0f));
+
+		sound = gameObject.GetComponent<AudioSource>();
+		sound.volume = 0.2f;
+		sound.Play ();
 	}
 	
 	// Update is called once per frame
